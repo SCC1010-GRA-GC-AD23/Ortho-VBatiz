e6dbe8911409b442bd23457d37b8fc35d0d62dc3
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-11-16 22:43:53 +0000

    Terminamos ejercicio

--- sketch.js
+++ sketch.js
@@ -5,13 +5,16 @@ function preload() {
 function setup() {
   // put setup code here
   createCanvas(800,800, WEBGL)
+  ortho()
 }
 
 function draw() {
   // put drawing code here
   background(255)
-  rotateX(frameCount * 0.01)
-  rotateY(frameCount * 0.01)
-  rotateZ(frameCount * 0.01)
+  //rotateX(frameCount * 0.01)
+  //rotateY(frameCount * 0.01)
+  //rotateZ(frameCount * 0.01)
+  box(150)
+  translate(100,100,-150)
   box(150)
 }
